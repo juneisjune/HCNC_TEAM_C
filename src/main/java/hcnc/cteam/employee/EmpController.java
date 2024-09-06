48fe59b8fcf57114e8dfc24be1d05c55a79fbce3
Yang Minjune <yhs1535@naver.com>
2024-09-06 15:39:11 +0900

    last project

--- src/main/java/hcnc/cteam/employee/EmpController.java
+++ src/main/java/hcnc/cteam/employee/EmpController.java
@@ -1,5 +1,24 @@
 package hcnc.cteam.employee;
 
-public class EmpController {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
+@Controller
+@RequestMapping(value="/test")
+public class EmpController {
+	
+	
+	@RequestMapping(value="/test11.do")
+	public String pglogin() {
+		System.out.println("123");
+//		ModelAndView view = new ModelAndView();
+//		view.setViewName("test");
+			
+		return "test01";
+	}
 }
