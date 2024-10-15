c62df320de7d33bee8d6e9577a4cb8111c4a8670
nagyeongjin <rudwls2080@gmail.com>
2024-10-15 10:04:35 +0900

    create HRD history

--- src/main/java/hcnc/cteam/employee/HrdMapper.java
+++ src/main/java/hcnc/cteam/employee/HrdMapper.java
@@ -14,4 +14,14 @@ public interface HrdMapper {
 	public void updateHRD(Map<String,Object> param);
 	
 	public void updateMng();
+	
+	public List<Map<String, Object>> getHrdHistory(Map<String,Object> param);
+	
+	public int selectHrdHistoryForDepOut(Map<String,Object> param);
+	
+	public int countHrdHistory(Map<String,Object> param);
+	
+	public void insertHrdHistory(Map<String, Object> param);
+	
+	public void updateDepOutIsNull(Map<String, Object> param);
 }
