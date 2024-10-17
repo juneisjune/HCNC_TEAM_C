e6be2d0bc950cbc52a19cb99f737e6821bf8da40
nagyeongjin <rudwls2080@gmail.com>
2024-10-17 12:04:44 +0900

    before merge

--- src/main/java/hcnc/cteam/employee/HrdController.java
+++ src/main/java/hcnc/cteam/employee/HrdController.java
@@ -3,6 +3,8 @@ package hcnc.cteam.employee;
 import java.util.List;
 import java.util.Map;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +39,7 @@ public class HrdController {
     }
 	
 	//변경사항 저장
+	@Transactional
 	@RequestMapping(value = "/hrdListUpdate.do")
     public NexacroResult confirmOff(@ParamDataSet(name = "ds_UpdateHrdlist", required = false) List<Map<String,Object>> param){
 		
